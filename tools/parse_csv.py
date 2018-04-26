d42979a2ac620e62603598d7e28e24c3bd470f57
Till Smejkal <till.smejkal@tu-dresden.de>
2018-04-26 14:52:59 +0200

    Add "WARNING" when printing matching error message

--- tools/parse_csv.py
+++ tools/parse_csv.py
@@ -129,7 +129,7 @@ for l in energy_lines:
     try:
         last_val = find_perf_counter(values, last_val, ts, MAX_DIFF)
     except MatchError as e:
-        print(str(e), file=sys.stderr)
+        print("WARNING: " + str(e), file=sys.stderr)
         continue
 
     if name in values[last_val]:
