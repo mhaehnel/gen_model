f06f67a7a43ba0633fa233fb9e6cbef359a6bf2b
Marcus Haehnel <mhaehnel@tudos.org>
2018-05-06 20:56:24 +0200

    Also log requested frequency in addition to actual

--- tools/freq_vs_voltage.sh
+++ tools/freq_vs_voltage.sh
@@ -2,9 +2,9 @@
 #Generates a CSV with the voltage for every frequency
 #Sadly this is dependent on load :/
 
-echo "Frequency,Voltage"
+echo "Frequency_req,Frequency_act,Voltage"
 for f in `cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies`; do
 	elab frequency $(($f/1000)) >/dev/null
 	sleep 2
-    echo "$(($(< /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)/1000)),$(< /sys/class/hwmon/hwmon2/in0_input)"
+    echo "$(($(< /sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed)/1000)),$(($(< /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)/1000)),$(< /sys/class/hwmon/hwmon2/in0_input)"
 done 
