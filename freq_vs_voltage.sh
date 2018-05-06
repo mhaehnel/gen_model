b9d7bd03293a441de18659eb59ecf1177dd1329d
Marcus Haehnel <mhaehnel@tudos.org>
2018-05-06 11:57:37 +0200

    Use sensible sysfs files instead of heavy wrappers
    Also no longer write to file. Let that be the responsibility of the user

--- freq_vs_voltage.sh
+++ freq_vs_voltage.sh
@@ -2,10 +2,10 @@
 #Generates a CSV with the voltage for every frequency
 #Sadly this is dependent on load :/
 
-( echo "Frequency,Voltage"
-  for f in `cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies`; do
+echo "Frequency,Voltage"
+for f in `cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies`; do
 	elab frequency $(($f/1000)) >/dev/null
 	sleep 2
-    echo "$(elab frequency | cut -f4 -d\ ),$(sensors | grep Vcore | cut -f2 -d+ | cut -f1 -d\ )"
-  done 
-) | tee freq_voltage.csv
+    echo "$(($(< /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)/1000)),$(< /sys/class/hwmon/hwmon2/in0_input)"
+    elab frequency
+done 
