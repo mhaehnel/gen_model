6be2f43a0d98fb599ee11c0d58b64a368fb46ccd
Till Smejkal <till.smejkal@tu-dresden.de>
2018-05-28 17:08:47 +0200

    Add eris measuring tools

---
+++ tools/bench_eris.sh
@@ -0,0 +1,292 @@
+#!/usr/bin/env bash
+
+## Global Variables
+# Whether or not turbo boost should be measured too
+TURBO_BOOST=${TURBO_BOOST:-0}
+
+# Number of different frequencies that should be tested
+FREQ_STEPS=${FREQ_STEPS:-6}
+
+# Number of different cpu counts that should be tested
+CPU_STEPS=${CPU_STEPS:-2}
+
+# Which technique should be used to generate the tested frequencies
+# Possible values are:
+# 'distance' (tries to even out distance between frequencies)
+# 'stepwise' (tries to even out distance between steps)
+FREQ_STEP_MODE=${FREQ_STEP_MODE:-distance}
+
+# The rate at which perf should report the counter values (measurement granularity)
+RATE_MS=${RATE_MS:-500}
+
+# The minimum runtime each benchmark should be executed at each configuration in seconds
+MIN_RUNTIME=${MIN_RUNTIME:-30}
+
+# The logfile for the output of elab
+ELAB_LOG=${ELAB_LOG:-/dev/null}
+
+# The directory where the intermediate output files of perf should be saved
+PERF_DIR=${PERF_DIR:-perf_csv}
+
+# The file where the final CSV output should be saved to
+CSV=${CSV:-bench.csv}
+
+# The directories where the script and the benchmark files are located
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd )"
+
+ERIS_DIR=${ERIS_DIR:-"/eris"}
+ERIS_CTRL_DIR=${ERIS_CTRL_DIR:-"/eris-ctrl"}
+
+# The additional events that we must count
+INSTR_EVENT=${E_INSTR:-instructions}
+CYCLE_EVENT=${E_CYCLE:-cpu-cycles}
+CYCLE_REF_EVENT=${E_CYCLE_REF:-ref-cycles}
+BRANCH_EVENT=${E_BRANCH:-cpu/event=0xC4,umask=0x0/u}
+CACHE_EVENT=${E_CACHE:-cache-references}
+MEMORY_EVENT=${E_MEMORY:-cache-misses}
+AVX_EVENT=${E_AVX:-cpu/event=0xC7,umask=0x3C/u}
+
+## Helper functions
+# Command to reverse arrays
+arr_reverse() { declare ARR="$1[@]"; declare -ga $1="( `printf '%s\n' "${!ARR}" | tac` )"; }
+
+
+## Main
+
+# Check requirements for this script to work
+command -v elab >/dev/null 2>&1 || { echo >&2 "'elab' has to be installed"; exit 1; }
+command -v perf >/dev/null 2>&1 || { echo >&2 "'perf' has to be installed"; exit 1; }
+perf list | grep "power/energy-cores" >/dev/null 2>&1 || { echo >&2 "need perf support to read RAPL counters"; exit 1; }
+
+if [ $(< /sys/devices/system/cpu/cpu0/cpufreq/scaling_driver) != "acpi-cpufreq" ]; then
+    if [ $(< /sys/devices/system/cpu/cpu0/cpufreq/scaling_driver) == "intel_pstate" ]; then
+        cat >&2 << EOF
+Currently running intel_pstate cpu scaling driver! Need to use the acpi-cpufreq cpu scaling driver!
+Possible fixes:
+  - disable intel_pstate driver via commandline (intel_pstate=disable)
+EOF
+    else
+        echo "Running a not-supported cpu scaling driver! Need to use acpi-cpufreq cpu scaling driver!"
+    fi
+    exit 1
+fi
+
+# Setup the script's internals
+all_freqs=( $(< /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies) )
+cpu_gov=$(< /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
+nr_cpus=$(grep "cpu cores" /proc/cpuinfo | head -n1 | cut -d: -f2 | sed 's/ //g')
+nr_hts=$(grep "siblings" /proc/cpuinfo | head -n1 | cut -d: -f2 | sed 's/ //g')
+
+[ $FREQ_STEPS -ge 2 ] || { echo >&2 "Must use at least 2 frequency steps (current: FREQ_STEPS=$FREQ_STEPS)"; exit 1; }
+[ $FREQ_STEPS -le ${#all_freqs[@]} ] || { echo >&2 "Must use less frequency steps than available frequencies (current: FREQ_STEPS=$FREQ_STEPS, frequencies=${#all_freqs[@]})"; exit 1; }
+[ $CPU_STEPS -ge 2 ] || { echo >&2 "Must use at least 2 CPU steps (current: CPU_STEPS=$CPU_STEPS)"; exit 1; }
+[ $CPU_STEPS -le $nr_cpus ] || { echo >&2 "Must use less CPU steps than available CPUs (current: CPU_STEPS=$CPU_STEPS, CPUs=$nr_cpus)"; exit 1; }
+
+# Check that we have enough permissions to make system-wide perf measurements.
+# To be able to do this we need CAP_SYS_ADMIN or perf need to be setup properly.
+if [ $EUID != 0 ]; then
+    # Not running as root is OK if perf_paranoid is set to -1
+    if [ $(< /proc/sys/kernel/perf_event_paranoid) -ne -1 ]; then
+        cat >&2 << EOF
+Need to be able to monitor system-wide perf events!
+Possible fixes:
+  - run script as root
+  - echo -1 > /proc/sys/kernel/perf_event_paranoid to allow all users to monitor system-wide pref events
+EOF
+        exit 1;
+    fi
+fi
+
+arr_reverse all_freqs
+
+# Remove the turbo frequency from the list of frequencies
+if (( ((${all_freqs[${#all_freqs[@]}-1]} / 1000) % 10) == 1 )); then
+    turbo_freq=${all_freqs[${#all_freqs[@]}-1]}
+    unset all_freqs[${#all_freqs[@]}-1]
+else
+    turbo_freq=
+fi
+min_freq=${all_freqs[0]}
+max_freq=${all_freqs[${#all_freqs[@]}-1]}
+
+declare -a freqs
+if [ $FREQ_STEP_MODE = stepwise ]
+then
+    #Find values in FREQ_STEP distance between indices
+    declare -a indices="(
+        $(
+            bc -l <<< "for (i=0; i < ${#all_freqs[@]}; i+=(${#all_freqs[@]}-1)/(${FREQ_STEPS}-1)) i" | 
+            xargs printf "%.0f\n"
+         ) )"
+    for i in ${indices[@]}; do freqs+=("${all_freqs[$i]}"); done
+elif [ $FREQ_STEP_MODE = distance ]
+then
+    freq_step=$((($max_freq - $min_freq)/($FREQ_STEPS - 1)))
+    freq=$min_freq
+    freqs=( $min_freq )
+    if [ $freq_step -gt 0 ]; then
+        for i in $(seq 1 $(($FREQ_STEPS - 2))); do
+            freq=$(($freq + $freq_step))
+            #Find closest existing freq ...
+            prev=${all_freqs[0]}
+            prev_dist=$(($prev-$freq))
+            for f in ${all_freqs[@]:1}; do
+                cur=$f
+                cur_dist=$(( $cur-$freq ))
+                if [ ${cur_dist#-} -gt ${prev_dist#-} ] #Are we getting farther away?
+                then
+                    freqs+=( $prev )
+                    break
+                fi
+                prev=$cur
+                prev_dist=$cur_dist
+            done
+            if [ $prev -eq ${all_freqs[-1]} ] 
+            then
+                freqs+=( $prev )
+            fi
+        done
+    fi
+    freqs+=( $max_freq )
+else
+    echo >&2 "Unknown FREQ_STEP_MODE ($FREQ_STEP_MODE)"; exit 1
+fi
+
+if [ $TURBO_BOOST -eq 1 ]; then
+    if [ "x$turbo_freq" == "x" ]; then
+        echo -ne "Turbo mode not supported by this processor\n" >&2
+    else
+        freqs+=( $turbo_freq )
+    fi
+fi
+
+cpu_step=$(($nr_cpus/$CPU_STEPS))
+cpu=1
+cpus="1"
+if [ $cpu_step -gt 0 ]; then
+    for i in $(seq 1 $(($CPU_STEPS - 2))); do
+        cpu=$(($cpu + $cpu_step))
+        cpus="$cpus $cpu"
+    done
+fi
+cpus="$cpus $nr_cpus"
+
+cat << EOF
+Detected system configuration:
+CPUs: $nr_cpus ($nr_hts HTs)
+Frequencies: $min_freq-$max_freq (@${cpu_gov}) {${all_freqs[@]}}
+Turbo: $(if [ "x$turbo_freq" == "x" ]; then echo "disabled"; else echo $turbo_freq; fi)
+
+Using the following steps:
+CPUs ($CPU_STEPS steps): $cpus
+Frequencies ($FREQ_STEPS steps): ${freqs[@]}
+
+Measurement rate: $RATE_MS ms
+EOF
+
+read -en1 -p "Continue? [Y/n] " answer
+case $answer in
+    N|n)
+        exit 0
+        ;;
+    Y|y|'')
+        ;;
+    *)
+        echo "Huh? Aborting";
+        exit 1
+        ;;
+esac
+
+# Fully abort the script upon CTRL-C
+trap "echo Aborting!; exit 0;" SIGINT SIGTERM
+
+mkdir -p $PERF_DIR
+
+exec 3<> $ELAB_LOG
+elab ht enable >&3
+elab frequency userspace >&3
+
+echo -ne "\nStart benchmarking\n"
+
+while read bench clients interval factor requests; do
+    echo -n "$bench: "
+
+    for ht in enable disable; do
+        if [ $ht == enable ]; then
+            echo -n "H"
+        fi
+
+        for cpu in $cpus; do
+            echo -n " $cpu"
+
+            for freq in ${freqs[@]}; do
+                echo -n "@$(($freq/1000))"
+
+                elab frequency $(($freq/1000)) >&3
+
+                if [ $ht == disable ]; then
+                    elab ht disable >&3
+                fi
+
+                file_ts=$(date +%Y_%m_%d-%H_%M_%S)
+                start_ts=$(date +%s)
+                iter=1
+
+                while (($(date +%s) - $start_ts < $MIN_RUNTIME)); do
+                    perf_counter_out_tmp="$(mktemp)"
+                    perf_counter_out="$PERF_DIR/${bench}.${ht}.${cpu}.${freq}.${file_ts}.${iter}.ctr.csv"
+                    perf_energy_out="$PERF_DIR/${bench}.${ht}.${cpu}.${freq}.${file_ts}.${iter}.energy.csv"
+                    eris_ctrl_out="$PERF_DIR/${bench}.${ht}.${cpu}.${freq}.${file_ts}.${iter}.eris.csv"
+
+                    if [ $ht == disable ]; then
+                        taskset_cpus="0-$(($cpu-1))"
+                        c=$cpu
+                    else
+                        taskset_cpus="0-$(($cpu-1)),$nr_cpus-$(($nr_cpus+$cpu-1))"
+                        c=$(($cpu*2))
+                    fi
+
+                    curdir=$(pwd)
+                    cd $ERIS_DIR
+
+                    taskset -c $taskset_cpus \
+                        perf stat -a -e power/energy-cores/,power/energy-ram/,power/energy-pkg/ -I $RATE_MS -x \; -o "$perf_energy_out" \
+                        perf stat -e $INSTR_EVENT,$CYCLE_EVENT,$CYCLE_REF_EVENT,$BRANCH_EVENT,$CACHE_EVENT,$MEMORY_EVENT,$AVX_EVENT -I $RATE_MS -x \; -o "$perf_counter_out_tmp" \
+                        build/eris >/dev/null &
+                    pid_ERIS=$!
+
+                    $ERIS_CTRL_DIR/eris-bench run --user=euf --passwd=euf --wait --csv --out=$eris_ctrl_out --duration=$MIN_RUNTIME --mode=timed --factor=$factor --clients=$clients --interval=$interval --requests=$requests $bench
+                    kill $pid_ERIS
+
+                    cd $curdir
+                    mv "$perf_counter_out_tmp" "$perf_counter_out"
+
+                    # rename the events to predefined names
+                    sed -i "s#${INSTR_EVENT}#instructions#g" $perf_counter_out
+                    sed -i "s#${CYCLE_EVENT}#cpu-cycles#g" $perf_counter_out
+                    sed -i "s#${CYCLE_REF_EVENT}#cpu-cycles-ref#g" $perf_counter_out
+                    sed -i "s#${BRANCH_EVENT}#branch-events#g" $perf_counter_out
+                    sed -i "s#${CACHE_EVENT}#cache-events#g" $perf_counter_out
+                    sed -i "s#${MEMORY_EVENT}#memory-events#g" $perf_counter_out
+                    sed -i "s#${AVX_EVENT}#avx-events#g" $perf_counter_out
+
+                    # We are done with the benchmark -- parse the perf output file
+                    $BASE_DIR/parse_csv_eris.py $bench $ht $c $freq $perf_counter_out $perf_energy_out $eris_ctrl_out -o $CSV --append
+
+                    iter=$(($iter + 1))
+                done
+
+                if [ $ht == disable ]; then
+                    elab ht enable >&3
+                fi
+            done
+        done
+    done
+    echo -ne "\n"
+done <<< $(cat << EOF
+deduplication-indexed 100 1000 500 1000
+deduplication-scan 100 1000 500 10
+tatp 100 1000 10 135
+ssb 1 1000 1 1
+EOF
+)
---
+++ tools/parse_csv_eris.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python3
+
+import sys
+import os.path
+import argparse
+
+
+def output_headers(headers, sep, outfile):
+    print(sep.join(headers), file=outfile)
+
+def output_row(values, headers, sep, outfile):
+    vals = []
+    for h in headers:
+        if h in values:
+            vals.append(str(values[h]))
+        else:
+            vals.append('')
+
+    print(sep.join(vals), file=outfile)
+
+class MatchError(RuntimeError):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+def match_energy_counter(values, last_best, current_ts, max_diff, force_next=False):
+    # Check if the one we used last time is still close enough
+    if not force_next and abs(values[last_best]["ts"] - current_ts) < max_diff:
+        return last_best
+
+    # Otherwise search starting from the last one another value that fits better
+    for i in range(last_best+1, len(values)):
+        if abs(values[i]["ts"] - current_ts) < max_diff:
+            return i
+
+    raise MatchError("Can't find performance counter match for energy value: {}".format(current_ts))
+
+def match_eris_counter(values, last_best, current_ts, max_diff):
+    # Check if the one we used last time is still close enough
+    if abs(values[last_best]["ts"] - current_ts) < max_diff:
+        return last_best
+
+    # Otherwise search starting from the last one another value that fits better
+    for i in range(last_best+1, len(values)):
+        if abs(values[i]["ts"] - current_ts) < max_diff:
+            return i
+
+    # If we still can't find any, we start searching from the beginning, maybe we find a match there
+    for i in range(0, last_best):
+        if abs(values[i]["ts"] - current_ts) < max_diff:
+            return i
+
+    raise MatchError("Can't find performance counter match for eris value: {}".format(current_ts))
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("bench", action="store", help="the benchmark name")
+parser.add_argument("ht", action="store", help="the hyperthreading mode")
+parser.add_argument("cpus", action="store", help="the number of cpus used")
+parser.add_argument("freq", action="store", help="the frequency used")
+parser.add_argument("ctrcsv", action="store", help="the perf csv-like file containing the counters")
+parser.add_argument("energycsv", action="store", help="the perf csv-like file containing the energy")
+parser.add_argument("eriscsv", action="store", help="the csv-like file containing the eris conturs")
+parser.add_argument("-o", "--outfile", action="store", default=None, help="save csv output to this file")
+parser.add_argument("--sep", action="store", default=";", help="use this separator (default: ;)")
+parser.add_argument("--append", action="store_true", default=False, help="append if the output file already exists")
+
+args = parser.parse_args()
+
+bench = args.bench
+ht = args.ht
+cpus = args.cpus
+freq = args.freq
+ctr_csv = args.ctrcsv
+energy_csv = args.energycsv
+eris_csv = args.eriscsv
+
+outfile = args.outfile
+sep = args.sep
+append = args.append
+
+try:
+    with open(ctr_csv) as perffile:
+        ctr_lines = perffile.read().splitlines()
+except IOError:
+    print("Can't open counter perf csv-file", file=sys.stderr)
+    sys.exit(1)
+
+try:
+    with open(energy_csv) as perffile:
+        energy_lines = perffile.read().splitlines()
+except IOError:
+    print("Can't open energy perf csv-file", file=sys.stderr)
+    sys.exit(1)
+
+try:
+    with open(eris_csv) as csvfile:
+        eris_lines = csvfile.read().splitlines()
+except IOError:
+    print("Can't open eris csv-file", file=sys.stderr)
+    sys.exit(1)
+
+# Parse the data from the perf csv-files
+values = []
+last_ts = None
+err_names = []
+# First read in the performance counter values
+for l in ctr_lines:
+    if l.startswith("#"):
+        # Skip the first line
+        continue
+
+    if  len(l) == 0:
+        # Skip empty lines
+        continue
+
+    eles = l.split(";")
+    ts = float(eles[0].strip())
+    name = eles[3]
+    value = eles[1].strip()
+
+    if last_ts == None:
+        values.append({ "ts" : ts, "t_diff" : ts, "bench" : bench, "ht" : ht, "cpus" : cpus, "freq" : freq })
+        last_ts = ts
+    elif ts != last_ts:
+        if err_names:
+            print("WARNING: uncounted value for {} in {} at time {} -- Skipping record".format(err_names,ctr_csv,last_ts),file=sys.stderr)
+            err_names = []
+            values = values[:-1]
+        values.append({ "ts" : ts, "t_diff" : ts - last_ts, "bench" : bench, "ht" : ht, "cpus" : cpus, "freq" : freq })
+        last_ts = ts
+    if value == "<not counted>":
+        err_names.append(name)
+
+    values[-1][name] = value
+
+# Next read in the RAPL counter values and try to match them with the performance counters
+MAX_DIFF = 0.02      # The maximum difference in the time stamps to still be counted equal (seconds)
+last_val = 0
+
+for l in energy_lines:
+    if l.startswith("#"):
+        # Skip the first line
+        continue
+
+    if len(l) == 0:
+        # Skip empty lines
+        continue
+
+    eles = l.split(";")
+    ts = float(eles[0].strip())
+    name = eles[3]
+    value = float(eles[1].strip())
+
+    try:
+        last_val = match_energy_counter(values, last_val, ts, MAX_DIFF)
+    except MatchError as e:
+        print("WARNING: " + str(e), file=sys.stderr)
+        continue
+
+    if name in values[last_val]:
+        # Try if the next performance counter value might fit as well
+        try:
+            last_val = match_energy_counter(values, last_val, ts, MAX_DIFF, True)
+        except MatchError as e:
+            print("WARNING: summing up duplicate energy value at time {} (energy timestamp: {})".format(values[last_val]["ts"], ts), file=sys.stderr)
+            values[last_val][name] += value
+    else:
+        values[last_val][name] = value
+
+# Now match the eris values to the perf values
+eris_values = []
+last_val = 0
+for l in eris_lins[1:]:
+    eles = l.split(";")
+    name = eles[0]
+    ts = float(eles[1].strip())/1000
+    value = float(eles[1].strip())
+
+    try:
+        last_val = match_eris_counter(values, last_val, ts, MAX_DIFF)
+    except MatchError as e:
+        print("WARNING: " + str(e), file=sys.stderr)
+
+    values[last_val][name] = value
+    if not last_val in eris_values:
+        eris_values.append(last_val)
+
+# Done parsing, create the output
+columns = list(values[0].keys())
+needs_header = True
+out = sys.stdout
+
+if outfile:
+    if append and os.path.isfile(outfile):
+        try:
+            out = open(outfile)
+            header = out.readline().rstrip('\n')
+
+            if len(header) != 0:
+                columns = header.split(sep)
+                needs_header = False
+
+            out.close()
+
+            out = open(outfile, "a")
+        except IOError:
+            print("Can't open output file for append")
+            sys.exit(1)
+    else:
+        try:
+            out = open(outfile, "w")
+        except IOError:
+            print("Can't open output file")
+            sys.exit(1)
+
+# Create the output
+if needs_header:
+    output_headers(columns, sep, out)
+for i,vals in enumerate(values):
+    if i in eris_values:
+        output_row(vals, columns, sep, out)
+
+if outfile:
+    out.close()
